d139798e295b44455b67b05a7f74f35e0567ff19
Don Roy Chacko <donisepic30@gmail.com>
2024-12-09 01:42:40 +0300

    websocket basic implementation, TODO make this better lol

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,13 @@ import http from 'http';
 import authRoutes from './routes/authRoutes.js';
 import timetableRoutes from './routes/timetableRoutes.js'
 import { Server } from "socket.io";
+import { setupSocketHandlers } from "./routes/socketRoutes.js";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server)
+
+setupSocketHandlers(io)
 
 app.use(express.json())
 app.use(authRoutes);
---
+++ backend/src/routes/socketRoutes.ts
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken"
+
+const connectedUsers = {}; //inshallah this one connectedUsers Objects scales
+
+export function setupSocketHandlers(io) {
+    // authenticate JWTs
+    io.use((socket, next) => {
+      const token = socket.handshake.auth?.jwt;
+  
+      if (!token) {
+        return next(new Error('Authentication error: Token required'));
+      }
+  
+      try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        socket.user = user.email; 
+        next();
+      } catch (err) {
+        next(new Error('Authentication error: Invalid token'));
+      }
+    });
+  
+    io.on('connection', (socket) => {
+      const user = socket.user;
+      console.log('User connected:', user);
+  
+      connectedUsers[user] = {
+        socketId: socket.id,
+      };
+  
+      console.log('Connected Users:', connectedUsers);
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected:', user);
+        delete connectedUsers[user];
+        console.log('Updated Users:', connectedUsers);
+      });
+    });
+  }
